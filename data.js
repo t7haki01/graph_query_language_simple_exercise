ffa02e290814cdfae5855cf765505e1a94e39195
t7haki01 <t7haki01@students.oamk.fi>
2019-02-26 09:11:34 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,120 @@
+
+module.exports = {
+    students :[
+        {
+            Firstname: "Owen",
+            Lastname: "Micheal",
+            Email:"micheal.owen@email.com",
+            Birthday: "14-12-1979",
+            Address: "Kotkantie 1",
+            Class: "LIV02",
+            enrolledCourses: [1,2,3],
+            Id: 1,
+            GroupId: 1,
+            alias: "MidgetGem"
+        },
+        {
+            Firstname: "Carrick",
+            Lastname: "Micheal",
+            Email:"micheal.carrick@email.com",
+            Birthday: "28-07-1981",
+            Address: "Torikatu 1",
+            Class: "MAN02",
+            enrolledCourses: [1,2,3],
+            Id: 2,
+            GroupId: 2,
+            alias: "Carras"
+        },
+        {
+            Firstname: "Messi",
+            Lastname: "Lionel",
+            Address: "Isokatu 1",
+            Email:"lionel.messi@email.com",
+            Birthday: "24-06-1987",
+            Class: "Bar06",
+            enrolledCourses: [1,2,3],
+            Id: 3,
+            GroupId: 3,
+            alias: "La Pulga"
+        }
+    ],
+    courses:[
+        {
+            Teacher: "Arsène Wenger",
+            Name: "How to kick the freekick",
+            Description: "This course explains how to kick the freekick in basic and practice kicking the freekick",
+            enrolledStudents: [1,2,3],
+            Id: 1
+        },
+        {
+            Teacher: "Kari Laitinen",
+            Name: "JAVA Basic",
+            Description: "This course is for the very beginner",
+            enrolledStudents: [1,2,3],
+            Id: 2
+        },
+        {
+            Teacher: "Kaija Posio",
+            Name: "Basic Finnish",
+            Description: "This course is for the people who wants to learn Finnish language",
+            enrolledStudents: [1,2,3],
+            Id: 3
+        }
+    ],
+    grades:[
+        {
+            Course: 1,
+            Student: 1,
+            Grade: 5,
+            Id: 1
+        },
+        {
+            Course: 1,
+            Student: 2,
+            Grade: 4,
+            Id: 2
+        },
+        {
+            Course: 1,
+            Student: 3,
+            Grade: 3,
+            Id: 3
+        },
+        {
+            Course: 2,
+            Student: 1,
+            Grade: 3,
+            Id: 4
+        },
+        {
+            Course: 2,
+            Student: 2,
+            Grade: 4,
+            Id: 5
+        },
+        {
+            Course: 2,
+            Student: 3,
+            Grade: 5,
+            Id: 6
+        },
+        {
+            Course: 3,
+            Student: 1,
+            Grade: 2,
+            Id: 7
+        },
+        {
+            Course: 3,
+            Student: 2,
+            Grade: 4,
+            Id: 8
+        },
+        {
+            Course: 3,
+            Student: 3,
+            Grade: 5,
+            Id: 9
+        },
+    ]
+}
---
+++ index.js
@@ -0,0 +1,428 @@
+let express = require('express');
+
+var data = require('./data');
+
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const app = express();
+
+const schema = gql`
+  type Mutation {
+    createStudent(
+      name: studentName!,
+      email: String!,
+      address: String!,
+      birthday: String!,
+      class: String!,
+    #   enrolledCourses: studentCourseInfo!,
+      groupId: Int!
+      ): createResponse!
+    ,
+    createCourse(
+      teacher: String!,
+      name: String!,
+      description: String!,
+      # enrolledStudents: 
+      ) : createResponse!
+    ,
+    createGrade(
+      course: Int!,
+      student: Int!,
+      grade: String!,
+    ) : createResponse!
+    ,
+    editStudent(
+      id: ID!,
+      name: studentName,
+      email: String,
+      address: String,
+      birthday: String,
+      groupId: Int,
+      class: String,
+      alias: String
+    ) : editResponse!
+    ,
+    editCourse(
+      id: ID!,
+      teacher: String,
+      description: String,
+      # enrolledStudents:
+      name: String
+    ) : editResponse!
+    ,
+    editGrade(
+      id: ID!,
+      student: Int!,
+      course: Int!,
+      grade: String
+    ) : editResponse!
+    ,
+    deleteStudent(
+      id:ID!
+    ) : deleteResponse!
+    ,
+    deleteCourse(
+      id:ID!
+    ) : deleteResponse!
+    ,
+    deleteGrade(
+      id:ID!
+    ) : deleteResponse!
+
+  }
+
+  type createResponse {
+    success: Boolean!    
+  }
+
+  type deleteResponse {
+    msg: String!,
+    result: Boolean!
+  }
+
+  type editResponse {
+    Firstname: String,
+    Lastname: String,
+    Email: String,
+    Birthday: String,
+    Address: String,
+    Class: String,
+    Id: ID!,
+    GroupId: Int,
+    alias: String,
+    Teacher: String,
+    Name: String,
+    Description: String,
+    Course: Int,
+    Student: Int,
+    Grade: String
+    msg: String!
+  }
+
+  type Query {
+    student(id: ID!): Student,
+    students: [Student!]!,
+    course(id: ID!): Course,
+    courses: [Course!]!,
+    grade(id: ID!): Grade,
+    grades: [Grade!]!,
+    # invoice(id: ID!): Invoice!
+    gradeInfo(id: ID!): GradeInfo,
+    gradesInfo: [GradeInfo!]!,
+    gradeByStudent(id: ID!): GradeByStudent,
+  }
+
+  type Student {
+    Id: ID!,
+    groupId: Int!,
+    # enrolledCourses: studentCourseInfo!,
+    Email: String!,
+    Name: Name!,
+    Address: String!,
+    Birthday: String!,
+    Class: String!,
+    alias: String #nickname
+  }
+
+  type Course{
+    Teacher: String!,
+    Name: String!,
+    Description: String!,
+    # enrolledCourses: ,
+    Id: ID!
+  }
+
+  type Grade{
+    Course: Int!,
+    Student: Int!,
+    Grade: String!,
+    Id: ID!
+  }
+
+#   type studentCourseInfo {
+
+#   }
+
+  type Name {
+      Firstname: String!,
+      Lastname: String!
+  }
+
+  input studentName {
+    Firstname: String!,
+    Lastname: String!
+  }
+
+  type GradeByStudent {
+    Id: ID!
+    gradeData: [gradeData]
+  }
+
+  type gradeData{
+    Course: String,
+    Student: String,
+    Grade: String,
+    Id: ID
+  }
+
+  type GradeInfo {
+    Id:ID!,
+    Course: Course,
+    Student: Student,
+    Grade: String
+  }
+#   type Invoice {
+#     id: ID!,
+#     createdDate: String!,
+#     paymentDate: String,
+#     customer: User!
+#     description: String!
+#   }
+`;
+
+const resolvers = {
+  Query: {
+    student: (parent, args, context, info) => {
+      return data.students.find(student => parseInt(student.Id) === parseInt(args.id));
+    },
+    students: (parents, args, context, info) => {
+      return data.students;
+    },
+    // invoice: (parents, args, context, info) => {
+    //   const invoice = invoices.find(i => i.id == args.id);
+    //   return invoice; 
+    // }
+    course: (parent, args, context, info) => {
+      return data.courses.find(course => course.Id === parseInt(args.id));
+    },
+    courses: (parent, args, context, info) => {
+      return data.courses;
+    },
+    grade: (parent, args, context, info) => {
+      return data.grades.find(grade => grade.Id === parseInt(args.id));
+    },
+    grades: (parent, args, context, info) => {
+      return data.grades;
+    },
+    gradeByStudent: (parent, args, context, info) => {
+      return data.grades.find(grade => grade.Student === parseInt(args.id));
+    },
+    gradeInfo: (parent, args, context, info) => {
+      return data.grades.find(grade => parseInt(grade.Id) === parseInt(args.id));
+    },
+    gradesInfo: (parent, args, context, info) => {
+      return data.grades;
+    }
+  },
+
+  Mutation: {
+    createStudent: (parent, args, context, info) => { 
+      let newStudent = {
+        Id: data.students.length+1,
+        Email: args.email,
+        Firstname: args.name.Firstname,
+        Lastname: args.name.Lastname,
+        Address: args.address,
+        Class: args.class,
+        // enrolledCourses: args.enrolledCourses,
+        GroupId : args.groupId,
+        Birthday: args.birthday,
+        alias: null
+      }
+      data.students.push(newStudent);
+      return { success: true }
+    },
+
+    createCourse: (parent, args, context, info) => {
+      let newCourse = {
+        Id: data.courses.length+1,
+        Teacher: args.teacher,
+        Name: args.name,
+        Description: args.description,
+        // enrolledCourses: 
+      }
+      data.courses.push(newCourse);
+      return { success: true }
+    },
+
+    createGrade: (parent, args, context, info) => {
+      let newGrade = {
+        Id: data.grades.length+1,
+        Course: args.course,
+        Student: args.student,
+        Grade: args.grade,
+      }
+      data.grades.push(newGrade);
+      return { success: true }
+    },
+
+    editStudent: (parent, args, context, info) => {
+      var index = data.students.findIndex(student=> student.Id === parseInt(args.id));
+
+      var modifiedStudent = {
+        Firstname: args.name.Firstname,
+        Lastname: args.name.Lastname,
+        Email: args.email,
+        Birthday: args.birthday,
+        Address: args.address,
+        Class: args.class,
+        Id: args.id,
+        GroupId: args.groupId,
+        alias: args.alias
+      };
+ 
+      data.students[index] = modifiedStudent ;
+
+      modifiedStudent = {
+        Firstname: args.name.Firstname,
+        Lastname: args.name.Lastname,
+        Email: args.email,
+        Birthday: args.birthday,
+        Address: args.address,
+        Class: args.class,
+        Id: parseInt(args.id),
+        GroupId: args.groupId,
+        alias: args.alias,
+        msg: "Student Data modified as above"
+      } 
+      return modifiedStudent ;
+    },
+
+    editCourse: (parent, args, context, info) => {
+
+      var index = data.courses.findIndex(course=> course.Id === parseInt(args.id));
+
+      var modifiedCourse = {
+        Teacher: args.teacher,
+        Name: args.name,
+        Description: args.description,
+        Id: parseInt(args.id)
+      };
+ 
+      data.courses[index] = modifiedCourse ;
+
+      modifiedCourse = {
+        Teacher: args.teacher,
+        Name: args.name,
+        Description: args.description,
+        Id: parseInt(args.id),
+        msg: "Course Data modified as above"
+      } 
+      return modifiedCourse ;
+    },
+    editGrade: (parent, args, context, info) => {
+
+      var index = data.grades.findIndex(grade=> grade.Id === parseInt(args.id));
+
+      var modifiedGrade = {
+        Course: parseInt(args.course),
+        Student: parseInt(args.student),
+        Grade: args.grade,
+        Id: parseInt(args.id)
+      };
+ 
+      data.grades[index] = modifiedGrade ;
+
+      modifiedGrade = {
+        Course: parseInt(args.course),
+        Student: parseInt(args.student),
+        Grade: args.grade,
+        Id: parseInt(args.id),
+        msg: "Grade Data modified as above"
+      } 
+      return modifiedGrade ;
+    },
+
+    deleteStudent: (parent, args, context, info) => {
+      var index = data.students.findIndex(student=>parseInt(student.Id) === parseInt(args.id));
+      data.students.splice(index,1);
+      return {
+              msg: "Student data which id is " + args.id + ", has been deleted",
+              result: true
+            }
+    },
+    deleteCourse: (parent, args, context, info) => {
+      var index = data.courses.findIndex(course=>parseInt(course.Id) === parseInt(args.id));
+      data.courses.splice(index,1);
+      return {
+              msg: "Course data which id is " + args.id + ", has been deleted",
+              result: true
+            }
+    },
+    deleteGrade: (parent, args, context, info) => {
+      var index = data.grades.findIndex(grade=>parseInt(grade.Id) === parseInt(args.id));
+      data.grades.splice(index,1);
+      return {
+              msg: "Grade data which id is " + args.id + ", has been deleted",
+              result: true
+            }
+    },
+
+  },
+
+  Student: {
+    Name: (parent, args, context, info) =>
+    {
+      return {
+        Firstname: parent.Firstname,
+        Lastname: parent.Lastname
+      }
+    },
+  }, 
+  GradeInfo: {
+    Student: (parent,args,context,info) => {
+      console.log("From student ", parent.Student);
+      return data.students.find(student => student.Id == parent.Student);
+    },
+    Course: (parent,args,context,info) => {
+      console.log("From course ", parent.Course);
+      return data.courses.find(course => course.Id == parent.Course);
+    }
+  },
+  GradeByStudent: {
+    gradeData: (parent, args, context, info) => {
+        var studentName = data.students.find(student => student.Id === parseInt(parent.Id)).Firstname +
+            " " + data.students.find(student => student.Id === parseInt(parent.Id)).Lastname ;
+        var gradeinfo = new Array();
+
+        for(var grade in data.grades){
+          if(data.grades[grade].Student === parseInt(parent.Id)){
+            var temp = {};
+            var courseName = data.courses.find(course => course.Id === parseInt(data.grades[grade].Course)).Name;
+            temp.Course = courseName;
+            temp.Student = studentName;
+            temp.Id = data.grades[grade].Id;
+            temp.Grade = data.grades[grade].Grade;
+            gradeinfo.push(temp);
+          }
+        }
+      return gradeinfo;
+    }
+  }
+
+//   Invoice: {
+//     customer: (parent, args, context, info) => {
+//       const user = users.find(u => u.id == parent.customer);
+//       return user;
+//     }
+//   }
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  formatError: error => {
+    console.log(error);
+    return error;
+  },
+  formatResponse: response => {
+    console.log(response);
+    return response;
+  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
